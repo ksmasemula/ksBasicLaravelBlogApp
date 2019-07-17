b4075570a38ee6f4ffa0e9068f063d08022ced27
Kabelo <ksmasemula@gmail.com>
2019-07-17 20:37:07 +0200

    Demo: Control Structures

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,15 @@
 @section('content')
 <div class="row">
     <div class="col-md-12">
-    <h1>Some Content </h1>
+    <h1>Control Structures </h1>
+    @if(true)
+    <p>This will only show if condition is true</p>
+    @else
+    <p>This will only show if condition is false </p>
+    @endif
+    @for($i =0;$i < 5 ; $i++)
+    <p>  {{$i + 1 }} . Iteration </p>
+    @endfor
     </div>
 </div>
 @endsection
