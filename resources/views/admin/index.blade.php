7b531bca586bd1a15d53e2673516c130871d7c4a
Kabelo <ksmasemula@gmail.com>
2019-07-17 22:25:16 +0200

    navigation routes paths part 2

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="row">
+        <div class="col-md-12">
+            <a href="{{route('admin.create')}}" class="btn btn-success">New Post</a>
+        </div>
+</div>
+<hr>
+<div class="row">
+        <div class="col-md-12">
+            <p><strong></strong>Learning Laravel <a href="{{route('admin.edit')}}">Edit</a></p>
+        </div>
+</div>
+@endsection
